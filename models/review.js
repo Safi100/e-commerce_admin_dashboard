5e624262eef29550398e88d736882457bd87d8d3
Safi100 <safinafi12@gmail.com>
2023-06-11 16:54:27 +0300

    create review shema

---
+++ models/review.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ReviewSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+       type: String,
+       required: true
+    },
+    rating : {
+       type: Number,
+       required: true,
+       min: 1,
+       max: 5
+    },
+    author : {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+},
+{timestamps: true}
+)
+
+module.exports = mongoose.model('Review', ReviewSchema)
