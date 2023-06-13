b070ce04e3585b92dbb66285a3ff4afe95e099d7
Safi100 <safinafi12@gmail.com>
2023-06-13 20:06:43 +0300

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const session = require('express-session')
 const authRoute = require('./routes/auth')
 const morgan = require('morgan')
-const flash = require('connect-flash')
 const { logout } = require('./controllers/auth')
 const {isLoggedIn} = require('./middleware')
+
 const indexPageDataFetch = require('./controllers/indexPageDataFetch')
 const app = express()
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.use(express.static('public'))
 app.use(morgan('dev'))
+app.use(cors())
 
 app.use(session({
     name: 'session',
@@ -26,7 +25,6 @@ app.use(session({
         expires : Date.now() + 1000 * 60 * 60 * 24 * 7
     }
 }))
-app.use(flash())
 
 mongoose.connect('mongodb://localhost:27017/Ecommerce', {
     useNewUrlParser: true, 
@@ -36,19 +34,13 @@ mongoose.connect('mongodb://localhost:27017/Ecommerce', {
 .then(()=> console.log("db connected"))
 .catch((err)=> console.log(err))
 
-app.use((req, res, next)=> {
-    res.locals.currentUser = req.session.user
-    res.locals.error = req.flash('error')
-    next()
-})
-
-app.get("/", isLoggedIn, indexPageDataFetch)
+app.get("/", indexPageDataFetch)
 
 app.use('/login', authRoute)
 
 app.get('/logout', logout)
 
 
-app.listen(3000, ()=> {
+app.listen(8000, ()=> {
     console.log("listening on port 3000")
 })
