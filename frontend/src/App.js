ab0b7563106ee88da0ec64bc6acd672230d0f747
Safi100 <safinafi12@gmail.com>
2023-06-13 20:06:34 +0300

    navigate to login if not logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
 import Login from './components/login/Login'
 import IndexPage from './components/indexPage/IndexPage'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ReactSession } from 'react-client-session';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom'
 const App = () => {
+    const Navigate = useNavigate()
+    ReactSession.setStoreType("localStorage");
+    useEffect(()=>{
+        const username = ReactSession.get("username");
+        if(!username){
+            return Navigate('/login')
+        }
+    },[])
     return (
-        <Router>
               <Routes>
                 <Route path='/login' element={<Login/>} />
                 <Route element={[<Navbar />, <Sidebar />]}>
@@ -14,7 +22,6 @@ const App = () => {
                 </Route>
                   {/* <Route path='*' element={<NotFound/>} /> */}
               </Routes>
-        </Router>
     );
 }
 
