d0bfef98928596ec4f686c2aab85d36e95138436
Safi100 <safinafi12@gmail.com>
2023-06-14 22:30:04 +0300

    edit loggedin middleware

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,7 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(req.session.user){
-        next()
-    }else{
-        return res.redirect('/login')
+        return next()
     }
+    console.log("please log in");
+    res.json(null)
 }
