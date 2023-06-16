c25b586dd332d5a054adcf243de70fc42c969dcf
Safi100 <safinafi12@gmail.com>
2023-06-16 22:23:20 +0300

    create fetch product middleware

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -4,3 +4,9 @@ module.exports.getProducts = async (req, res) => {
     const products = await Products.find()
     res.json(products)
 }
+
+module.exports.productProfie = async (req, res) => {
+    const id = req.params.id
+    const product = await Products.findById(id)
+    res.json(product)
+}
