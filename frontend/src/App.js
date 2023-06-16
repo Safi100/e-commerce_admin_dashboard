8d6f0e32555952c59cf20153c2f7adcfd26311be
Safi100 <safinafi12@gmail.com>
2023-06-16 22:23:54 +0300

    add more routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import Sidebar from './components/sidebar/Sidebar';
 import Login from './components/login/Login'
 import CustomersPage from './components/customersPage/CustomersPage'
 import IndexPage from './components/indexPage/IndexPage'
+import Products from './components/products/Products';
 import { ReactSession } from 'react-client-session';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom'
+import ProductProfile from './components/products/ProductProfile';
 const App = () => {
     const Navigate = useNavigate()
     ReactSession.setStoreType("localStorage");
@@ -18,9 +20,11 @@ const App = () => {
         <>
             {window.location.pathname !== '/login' && <Sidebar />}
               <Routes>
+                <Route exact path='/' element={<IndexPage/>} />
                 <Route path='/login' element={<Login/>} />
                 <Route path='/customers' element={<CustomersPage/>} />
-                <Route exact path='/' element={<IndexPage/>} />
+                <Route path='/products' element={<Products/>} />
+                <Route path='/products/:id' element={<ProductProfile/>} />
                   {/* <Route path='*' element={<NotFound/>} /> */}
               </Routes>
         </>
