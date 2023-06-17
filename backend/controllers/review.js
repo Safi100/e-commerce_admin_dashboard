1c62ba5a9d9b7d75e942957ce4e5cf70474a58a6
Safi100 <safinafi12@gmail.com>
2023-06-17 19:44:17 +0300

    get review by (newest/top&low rating) middleware

---
+++ backend/controllers/review.js
@@ -0,0 +1,25 @@
+const Review = require('../models/review')
+
+module.exports.GetReviews = async (req, res) => {
+    var review
+    const {orderBy} = req.query
+    console.log(JSON.stringify(orderBy));
+    switch (orderBy) {
+        case "newest":
+            console.log("this");
+            review = await Review.find({}).sort({createdAt: -1})
+        break;
+        case "top":
+            review = await Review.find({}).sort({rating: -1})
+        break;
+        case "lowest":
+            review = await Review.find({}).sort({rating: 1})
+        break;
+    
+        default:
+            review = await Review.find({})
+        break;
+    }
+    res.json(review)
+}
+
