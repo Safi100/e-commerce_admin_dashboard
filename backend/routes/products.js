3c6c709f3d7853482c70307d6cfd8798bc8f8792
Safi100 <safinafi12@gmail.com>
2023-06-17 23:54:25 +0300

    create get route for product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { getProducts } = require('../controllers/products')
+const { getProducts, productProfie } = require('../controllers/products')
 const router = express.Router({mergeParams: true})
 
 router.route('/')
 .get(getProducts)
+
+router.route('/:id')
+.get(productProfie)
 
 
 module.exports = router
