b506bb0be9d4e21658a1267e90a58f97c55249c1
Safi100 <safinafi12@gmail.com>
2023-06-18 20:05:55 +0300

    create category route

---
+++ backend/routes/category.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const { getCategories } = require('../controllers/category')
+const router = express.Router({mergeParams: true})
+
+router.route('/')
+.get(getCategories)
+
+module.exports = router
