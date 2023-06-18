606e5ce9a64620eb8eb81da1400b4ca393f899c6
Safi100 <safinafi12@gmail.com>
2023-06-18 20:06:49 +0300

    create filter by category on review controller

--- backend/controllers/review.js
+++ backend/controllers/review.js
@@ -1,24 +1,27 @@
 const Review = require('../models/review')
 module.exports.GetReviews = async (req, res) => {
     var review
-    const {orderBy} = req.query
-    console.log(JSON.stringify(orderBy));
+    const {orderBy, category} = req.query
     switch (orderBy) {
         case "newest":
-            console.log("this");
-            review = await Review.find({}).sort({createdAt: -1})
+            review = await Review.find().populate('author').populate({path: 'product', populate: {path:'category'}}).sort({createdAt: -1})
         break;
         case "top":
-            review = await Review.find({}).sort({rating: -1})
+            review = await Review.find().populate('author').populate({path: 'product', populate: {path:'category'}}).sort({rating: -1})
         break;
         case "lowest":
-            review = await Review.find({}).sort({rating: 1})
+            review = await Review.find().populate('author').populate({path: 'product', populate: {path:'category'}}).sort({rating: 1})
         break;
     
         default:
-            review = await Review.find({}).populate('author').populate('product')
-        break;
-    }
-    res.json(review)
+            review = await Review.find().populate('author').populate({path: 'product', populate: {path:'category'}})
+            break;
+        }
+        if(category !==""){
+            const filteredDocumentsByCategory = review.filter(doc => doc.product.category.CategoryName === `${category}`);
+            res.json(filteredDocumentsByCategory)
+        }else{
+            res.json(review)
+        }
 }
 
