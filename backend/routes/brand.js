087c3b3cebc136e4d03aa463afeaaec9a698a255
Safi100 <safinafi12@gmail.com>
2023-06-19 22:23:58 +0300

    create post route

--- backend/routes/brand.js
+++ backend/routes/brand.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { getBrands } = require('../controllers/brand')
+const { getBrands, newBrand } = require('../controllers/brand')
 const router = express.Router({mergeParams: true})
 
 router.route('/')
 .get(getBrands)
+.post(newBrand)
 
 module.exports = router
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { getCategories } = require('../controllers/category')
+const { getCategories, newCategory } = require('../controllers/category')
 const router = express.Router({mergeParams: true})
 
 router.route('/')
 .get(getCategories)
+.post(newCategory)
 
 module.exports = router
