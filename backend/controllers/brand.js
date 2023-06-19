6b5276a9512ab62bf977e406a141a8a0c2ac4f4f
Safi100 <safinafi12@gmail.com>
2023-06-19 22:24:33 +0300

    create new (category/brand) conteoller

--- backend/controllers/brand.js
+++ backend/controllers/brand.js
@@ -4,3 +4,15 @@ module.exports.getBrands = async (req, res) => {
     const brands = await Brand.find()
     res.send(brands)
 }
+module.exports.newBrand = async (req, res) => {
+    try{
+        const { brand } = req.body
+        const newBrand = new Brand({
+            BrandName: brand.toLowerCase()
+        })
+        await newBrand.save()
+        res.json(newBrand)
+    }catch(err){
+        return res.status(400).json(JSON.stringify(err.message))
+    }
+}
--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -4,3 +4,16 @@ module.exports.getCategories = async (req, res) => {
     const categories = await Category.find()
     res.send(categories)
 }
+
+module.exports.newCategory = async (req, res) => {
+    try{
+        const { category } = req.body
+        const newCategory = new Category({
+            CategoryName: category.toLowerCase()
+        })
+        await newCategory.save()
+        res.json(newCategory)
+    }catch(err){
+        return res.status(400).json(JSON.stringify(err.message))
+    }
+}
