eb7c88c15a4ff4189c6c6a5f83600084c4898212
Safi100 <safinafi12@gmail.com>
2023-06-20 23:26:20 +0300

    setup multer

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,8 +1,24 @@
-const  Products = require("../models/product")
+const Products = require("../models/product")
+const multer  = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/uploads/images')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+const upload = multer({ storage: storage })
 
 module.exports.getProducts = async (req, res) => {
     const products = await Products.find()
     res.json(products)
+}
+
+module.exports.createProduct = async (req, res) => {
+    
 }
 
 module.exports.productProfie = async (req, res) => {
