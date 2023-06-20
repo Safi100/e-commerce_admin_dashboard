015ed5ba5ff200dbe50812dd594bf3d6dfa03a6d
Safi100 <safinafi12@gmail.com>
2023-06-20 23:28:19 +0300

    add more data export from index data fetch

--- backend/controllers/indexPageDataFetch.js
+++ backend/controllers/indexPageDataFetch.js
@@ -1,13 +1,13 @@
 const customer = require("../models/customer");
-
-module.exports = async (req, res) => {
+const product = require('../models/product')
+module.exports.getIndexData = async (req, res) => {
     // customer count
     const CustomerCount = await customer.countDocuments()
     const now = new Date();
     const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
     // new customers
-    const NewCustomers = await customer.find({createdAt: {$gte: today}})
+    const NewCustomers = await customer.find({createdAt: {$gte: today}}).limit(10)
+    const productsCount = await product.countDocuments()
     // todo : latest orders
-    res.status(200).json(CustomerCount)
-    // res.render("index", {title: "Dashboard", CustomerCount, NewCustomers})
+    return res.json([CustomerCount, NewCustomers, productsCount])
 }
