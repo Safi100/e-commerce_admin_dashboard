b55868fcf22d6112dd3e4421ace51f74302e0109
Safi100 <safinafi12@gmail.com>
2023-06-23 23:38:31 +0300

    create  new product controller

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -10,11 +10,19 @@ module.exports.getProducts = async (req, res) => {
 
 module.exports.createProduct = async (req, res) => {
     const product = req.body
-    console.log(req.files);
-    console.log(product);
     const newProduct = new Product({
-        title: ""
+        title: product.title,
+        price: product.price,
+        discount: product.discount,
+        description: product.description,
+        chose_for_you: product.chose_for_you,
+        still_available: product.still_available,
+        category: product.category,
+        brand: product.brand,
+        images: req.files.map(file => ({url: file.path, filename: file.filename}))
     })
+    await newProduct.save()
+    res.send(newProduct)
 }
 
 module.exports.productProfie = async (req, res) => {
