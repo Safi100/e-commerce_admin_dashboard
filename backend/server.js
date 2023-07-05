32b13b3e74c7c15b20f38ff417ca49fccb7513af
Safi100 <safinafi12@gmail.com>
2023-07-05 19:36:30 +0300

    import env

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
@@ -17,6 +21,7 @@ const Review = require('./models/review')
 const Product = require('./models/product')
 const {getIndexData} = require('./controllers/indexPageDataFetch')
 const { getCustomers } = require('./controllers/customers')
+const product = require('./models/product')
 const app = express()
 
 app.use(express.json())
@@ -46,7 +51,8 @@ app.use((req, res, next) => {
     console.log(req.session) ;
     next()
 })
-app.get("/", getIndexData)
+
+app.get('/', getIndexData)
 app.get('/customers', getCustomers)
 
 app.use('/login', authRoute)
@@ -54,38 +60,8 @@ app.use('/products', productRoute)
 app.use('/reviews', ReviewRoute)
 app.use('/category', categoryRoute)
 app.use('/brand', brandRoute)
-app.post('/test', (req, res) => {
-    console.log(req.body)
-})
-app.get('/addReview', async (req, res) => {
-    const rev = new Review({
-        title:"Excelente",
-        body: "lorem lorem lorem lorem lorem lorem lorem lorem lorem ipsum",
-        rating: 5,
-        author: "648669b54064b12d378d19b9",
-        product: "648b36eda6e8114e11204343"
-    })
-    const product = await Product.findById('648b36eda6e8114e11204343')
-    product.reviews.push(rev)
-    await product.save()
-    await rev.save()
-    res.send(rev)
-})
-app.use('/addProduct', async (req, res) => {
-    const product = new Product({
-        title: "xbox 360",
-        description: "new xbox 360 with 10 games gift",
-        price: 250,
-        discount: 25,
-        category: "6485d4b114c750caff2b9530",
-        brand: "6485d5350c89919a49396f30"
 
-    })
-    await product.save()
-    res.send(product)
-})
 app.get('/logout', logout)
-
 
 app.listen(8000, ()=> {
     console.log("listening on port 3000")
