dbe4d83896799b2586a3b7829298856a634da5d1
Safi100 <safinafi12@gmail.com>
2023-07-05 19:35:53 +0300

    create updateProduct and remove images middleware

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,5 +1,5 @@
 const Product = require("../models/product")
-
+const {cloudinary} = require('../cloudinary/index')
 module.exports.getProducts = async (req, res) => {
     var products
     const {orderBy, category} = req.query
@@ -46,13 +46,32 @@ module.exports.createProduct = async (req, res) => {
     res.send(newProduct)
 }
 module.exports.updateProduct = async (req, res) => {
-    const id = req.params.id
-    const product = req.body
-    const UpdatedProduct = Product.findById(id)
-    // if(UpdatedProduct.images.length ){
-
-    // }
-    console.log(UpdatedProduct)
+    try{
+        const id = req.params.id
+        const product = req.body
+        const UpdatedProduct = await Product.findByIdAndUpdate(id, {
+            title: product.title,
+            price: product.price,
+            discount: product.discount,
+            description: product.description,
+            chose_for_you: product.chose_for_you,
+            still_available: product.still_available,
+            category: product.category,
+            brand: product.brand,
+        })
+        UpdatedProduct.priceToPay = product.price - (product.discount / 100) * product.price;
+        const imgs = req.files.map(file => ({url: file.path, filename: file.filename}))
+        UpdatedProduct.images.push(...imgs)
+        
+        console.log(UpdatedProduct);
+        if(UpdatedProduct.images.length > 4){
+            return res.status(409).json("You can't have more than 4 images")
+        }
+        await UpdatedProduct.save()
+        res.status(200).json(UpdatedProduct)
+    }catch(e){
+        return res.status(409).json(e)
+    }
 }
 module.exports.productProfie = async (req, res) => {
     const id = req.params.id
@@ -61,4 +80,21 @@ module.exports.productProfie = async (req, res) => {
         return res.status(404).json("Product not found")
     }
     res.json(product)
+}
+module.exports.deleteImages =  async (req, res) => {
+    try{
+        const id = req.params.id
+        const product = await Product.findByIdAndUpdate(id)
+        if(req.body.images){
+            for(let filename of req.body.images){
+                console.log(filename);
+                await cloudinary.uploader.destroy(filename)
+            }
+            await product.updateOne({$pull: {images: { filename: { $in : req.body.images } } } } )
+            await product.save()
+            res.status(200).json(product.images)
+        }
+    }catch(e){
+        console.log(e);
+    }
 }
