518955ac4d4e90a7707a156db84ca6ccf3b9d7ae
Safi100 <safinafi12@gmail.com>
2023-07-10 22:17:04 +0300

    add upload.single to category post

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const { getCategories, newCategory } = require('../controllers/category')
+const multer = require('multer')
+const { storage } = require('../cloudinary') 
+const upload = multer({storage})
 const router = express.Router({mergeParams: true})
 
 router.route('/')
 .get(getCategories)
-.post(newCategory)
+.post(upload.single('categoryImage'), newCategory)
 
 module.exports = router
