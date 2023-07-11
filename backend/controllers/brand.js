aa28cf3492531989d6654345546bd77b54e5e69d
Safi100 <safinafi12@gmail.com>
2023-07-11 21:27:27 +0300

    create fetch brands order by count

--- backend/controllers/brand.js
+++ backend/controllers/brand.js
@@ -1,8 +1,13 @@
 const Brand = require('../models/brand')
-
+const Product = require('../models/product')
 module.exports.getBrands = async (req, res) => {
     const brands = await Brand.find()
     res.send(brands)
+}
+module.exports.getBrandWithCount = async(req, res) => {
+    const brands = await Brand.find().populate('products')
+    brands.sort((a, b) => b.products.length - a.products.length);
+    res.json(brands);
 }
 module.exports.newBrand = async (req, res) => {
     try{
