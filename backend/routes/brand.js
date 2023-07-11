a14c15298a95390a8ec17aa8d26c7e9ede6748d8
Safi100 <safinafi12@gmail.com>
2023-07-11 21:29:27 +0300

    create brand coute route

--- backend/routes/brand.js
+++ backend/routes/brand.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { getBrands, newBrand } = require('../controllers/brand')
+const { getBrands, newBrand, getBrandWithCount } = require('../controllers/brand')
 const router = express.Router({mergeParams: true})
 
 router.route('/')
 .get(getBrands)
 .post(newBrand)
+
+router.route('/getBrandWithCount')
+.get(getBrandWithCount)
 
 module.exports = router
