f3b0a7248ce33dc9a5175db0266a59d4ebaa13a1
Safi100 <safinafi12@gmail.com>
2023-07-18 01:09:34 +0300

    create delete Advertisement controller

--- backend/controllers/advertisement.js
+++ backend/controllers/advertisement.js
@@ -1,4 +1,5 @@
 const Advertisement = require('../models/advertisement')
+const {cloudinary} = require('../cloudinary/index')
 
 module.exports.getAdvertisement = async (req, res) => {
     const advertisements = await Advertisement.find()
@@ -15,4 +16,17 @@ module.exports.newAdvertisements = (req, res) => {
     })
     newAdvertisements.save()
     res.status(200).json(newAdvertisements)
+}
+module.exports.deleteAdvertisements =  async (req, res) => {
+    try{
+        const DeletedAdvertisement = await Advertisement.find({ _id: { $in: req.body } })
+        DeletedAdvertisement.forEach(async Advertisement => {
+            console.log(Advertisement.image.filename);
+            await cloudinary.uploader.destroy(Advertisement.image.filename)
+        });
+        await Advertisement.deleteMany({_id: {$in: req.body} })
+        res.json(true)
+    }catch(e){
+        console.log(e);
+    }
 }
