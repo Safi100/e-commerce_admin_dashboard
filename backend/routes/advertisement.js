8d76c8eb1e8c19d75aae7f9ca742206d4305d693
Safi100 <safinafi12@gmail.com>
2023-07-18 01:09:43 +0300

    create delete Advertisement route

--- backend/routes/advertisement.js
+++ backend/routes/advertisement.js
@@ -1,7 +1,15 @@
 const express = require('express')
-const { getAdvertisement, newAdvertisements} = require('../controllers/advertisement')
+const { getAdvertisement, newAdvertisements, deleteAdvertisements} = require('../controllers/advertisement')
 const router = express.Router({mergeParams: true})
+const multer = require('multer')
+const { storage } = require('../cloudinary')
+const upload = multer({storage})
 
 router.route('/')
 .get(getAdvertisement)
 .post(upload.single('advertisementImage'), newAdvertisements)
+
+router.route('/delete')
+.post(deleteAdvertisements)
+
+module.exports = router
