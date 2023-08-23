608539a794070e7c5a940300a93879e8494bcedc
safi100 <safinafi12@gmail.com>
2023-08-23 16:53:55 +0300

    add AuthContextProvider to app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <AuthContextProvider>
     <Router>
       <App />
     </Router>
+  </AuthContextProvider>
 );
