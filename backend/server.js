f658ae0aaebf7e77e00c11193532ac6b8c4d6801
safi100 <safinafi12@gmail.com>
2023-08-23 16:53:02 +0300

    Add authenticateJWT to all routes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
 
 const authRoute = require('./routes/auth')
 const productRoute = require('./routes/products')
@@ -15,31 +16,18 @@ const categoryRoute = require('./routes/category')
 const brandRoute = require('./routes/brand')
 const advertisementRoute = require('./routes/advertisement')
 
-const morgan = require('morgan')
 const { logout } = require('./controllers/auth')
-// const {isLoggedIn} = require('./middleware')
-const Review = require('./models/review')
-const Product = require('./models/product')
+const { authenticateJWT } = require('./middleware');
 const {getIndexData} = require('./controllers/indexPageDataFetch')
 const { getCustomers } = require('./controllers/customers')
-const product = require('./models/product')
+
 const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
+app.use(cookieParser())
 app.use(express.static('uploads'))
-
-app.use(session({
-    name: 'session',
-    secret: 'thisShouldBeAbetterSecret',
-    resave: false,
-    saveUninitialized: false,
-    cookie : {
-        maxAge : 1000 * 60 * 60 * 24 * 1,
-        expires : Date.now() + 1000 * 60 * 60 * 24 * 7
-    }
-}))
 
 mongoose.connect('mongodb://localhost:27017/Ecommerce', {
     useNewUrlParser: true, 
@@ -48,20 +36,15 @@ mongoose.connect('mongodb://localhost:27017/Ecommerce', {
 })
 .then(()=> console.log("db connected"))
 .catch((err)=> console.log(err))
-app.use((req, res, next) => {
-    console.log(req.session) ;
-    next()
-})
-
-app.get('/', getIndexData)
-app.get('/customers', getCustomers)
+app.get('/', authenticateJWT,getIndexData)
+app.get('/customers', authenticateJWT, getCustomers)
 
 app.use('/login', authRoute)
-app.use('/products', productRoute)
-app.use('/reviews', ReviewRoute)
-app.use('/category', categoryRoute)
-app.use('/brand', brandRoute)
-app.use('/advertisement', advertisementRoute)
+app.use('/products', authenticateJWT, productRoute)
+app.use('/reviews', authenticateJWT, ReviewRoute)
+app.use('/category', authenticateJWT, categoryRoute)
+app.use('/brand', authenticateJWT, brandRoute)
+app.use('/advertisement', authenticateJWT, advertisementRoute)
 
 app.get('/logout', logout)
 
