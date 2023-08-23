9eea7dd1f614da0457e8cd6b05e52b4f3f6b2eb3
safi100 <safinafi12@gmail.com>
2023-08-23 16:51:51 +0300

    Create authenticateJWT middleware

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,7 +1,20 @@
-module.exports.isLoggedIn = (req, res, next) => {
-    if(req.session.user){
-        return next()
+const jwt = require('jsonwebtoken')
+
+module.exports.authenticateJWT = (req, res, next) => {
+    const AuthHeader = req.headers.authorization
+    console.log(req.headers);
+    const secret_key = process.env.SECRET_KEY
+    if(AuthHeader){
+        const token = AuthHeader.split(" ")[1]
+        jwt.verify(token, secret_key, (err, user) => {
+            if (err) {
+                return res.status(403).json({ error: 'Token is not valid!' });
+            }
+            req.user = user;
+            next();
+        });
+    }else{
+        return res.status(401).json({ error: 'Unauthorized' });
     }
-    console.log("please log in");
-    res.json(null)
+
 }
