fc5afa88f02a675075d5e3917d537f8de6a043ca
safi100 <safinafi12@gmail.com>
2023-08-23 16:53:31 +0300

    Create jwt token for login and handle login

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,14 +1,29 @@
 const bcrypt = require('bcrypt')
 const Admin = require('../models/admin')
+const jwt = require('jsonwebtoken');
 
 module.exports.login = async (req, res) => {
     const {username, password} = req.body
-    const admin = await Admin.findOne({username: username.trim()})
-    if(admin && (await bcrypt.compare(password.trim(), admin.password))){
-        req.session.user = admin.username
-        return res.json(admin.username)
-    }else{
-        return res.json(null)
+    const secret_key = process.env.SECRET_KEY
+    try{
+        const admin = await Admin.findOne({username: username.trim()})
+        if(admin && (await bcrypt.compare(password.trim(), admin.password))){
+
+            const token = jwt.sign({ username: admin.username }, secret_key, {
+                expiresIn: '7d' // Token expiration time
+            });
+            res.cookie('accessToken', token, {
+                httpOnly: true
+            }).status(200).json({username, token})
+            
+        }else{
+            res.status(400).json({
+                success: false,
+                message: "Username/password wrong", 
+            })
+        }
+    }catch (error) {
+        return res.status(500).json({ error: 'Server error' });
     }
 }
 module.exports.logout = (req, res) => {
