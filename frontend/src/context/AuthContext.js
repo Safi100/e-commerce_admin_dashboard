9ee8f61fa312664f33852c5dad2c17abeba8cf7a
safi100 <safinafi12@gmail.com>
2024-06-10 23:05:12 +0300

    update on auth context

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,61 +1,56 @@
-import { createContext, useEffect, useReducer } from "react";
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
+import axios from 'axios';
 
-const INITIAL_STATE = {
-  user: JSON.parse(localStorage.getItem("user")) || null,
-  loading: false,
-  error: null,
-};
+export const AuthContext = createContext([])
 
-export const AuthContext = createContext(INITIAL_STATE)
-
-const AuthReducer = (state, action) => {
-  switch (action.type) {
-    case "LOGIN_START":
-      return {
-        user: null,
-        loading: true,
-        error: null,
-      };
-    case "LOGIN_SUCCESS":
-      return {
-        user: action.payload,
-        loading: false,
-        error: null,
-      };
-    case "LOGIN_FAILURE":
-      return {
-        user: null,
-        loading: false,
-        error: action.payload,
-      };
-    case "LOGOUT":
-      return {
-        user: null,
-        loading: false,
-        error: null,
-      };
-    default:
-      return state;
-  }
-};
-
-export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+export function AuthContextProvider({ children }) {
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState(false)
+  const [currentUser, setCurrentUser] = useState(null)
 
   useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(state.user));
-  }, [state.user]);
+    fetchCurrentUser();
+  }, []);
+  
+  const fetchCurrentUser = () => {
+    if (!Cookies.get('c_user')) return;
+    axios.get('http://localhost:8000/auth/currentUser')
+      .then((res) => {
+        setCurrentUser(res.data)
+      })
+      .catch((e) => {
+        console.log(e)
+      });
+  }
+
+  const login = (email, password) => {
+    setError('')
+    setSuccess(false);
+    axios.post('http://localhost:8000/auth/login', { email, password })
+    .then((res) => {
+      fetchCurrentUser()
+      setSuccess(true)
+      setCurrentUser(res.data)
+    })
+    .catch((err) => {
+      setError(err.response.data.error)
+      console.log(err)
+    })
+  }
+  const logout = () => {
+    axios.post('http://localhost:8000/user/logout')
+    .then(() => setCurrentUser(null))
+    .catch((err) => console.log(err))
+  }
 
   return (
-    <AuthContext.Provider
-      value={{
-        user: state.user,
-        loading: state.loading,
-        error: state.error,
-        dispatch,
-      }}
-    >
+    <AuthContext.Provider value={{ login, logout, error, currentUser, success }}>
       {children}
     </AuthContext.Provider>
   );
-};
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
