e42916ee5d5d51b29f2ee03b3f880411205e1d75
safi100 <safinafi12@gmail.com>
2024-06-10 22:28:04 +0300

    update login and logout

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,32 +3,30 @@ const Admin = require('../models/admin')
 const jwt = require('jsonwebtoken');
 
 module.exports.login = async (req, res) => {
-    const {username, password} = req.body
-    const secret_key = process.env.SECRET_KEY
     try{
-        const admin = await Admin.findOne({username: username.trim()})
-        if(admin && (await bcrypt.compare(password.trim(), admin.password))){
+        const email = req.body.email.toLowerCase().trim()
+        const password = req.body.password.trim()
 
-            const token = jwt.sign({ username: admin.username }, secret_key, {
-                expiresIn: '7d' // Token expiration time
-            });
-            res.cookie('accessToken', token, {
-                httpOnly: true
-            }).status(200).json({username, token})
-            
-        }else{
-            res.status(400).json({
-                success: false,
-                message: "Username/password wrong", 
-            })
-        }
-    }catch (error) {
-        return res.status(500).json({ error: 'Server error' });
+        const admin = await Admin.findOne(email)
+        if(!admin) throw new Error('Email or password incorrect')
+        const match = await bcrypt.compare(password, admin.password)
+        if(!match) throw new Error('Email or password incorrect')
+
+        const token = jwt.sign({ id: admin._id }, process.env.SECRET_KEY, {
+            expiresIn: '30d' // Token expiration time
+        });
+        res.cookie('c_user', admin._id.toString());
+        res.cookie('accessToken', token, { httpOnly: true }).status(200).json({token});
+    }catch (e) {
+        return res.status(401).json({error: e.message});
     }
 }
-module.exports.logout = (req, res) => {
-    if(req.session.user){
-        req.session.destroy();
+module.exports.logout = async (req, res) => {
+    try{
+        await res.clearCookie('accessToken');
+        await res.clearCookie('c_user');
+        res.status(200).send({success: true})
+    }catch (e) {
+        res.status(401).json({error: e.message})
     }
-    res.json(true)
 }
