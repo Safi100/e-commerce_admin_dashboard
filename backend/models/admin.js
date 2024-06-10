d10e3c78c900e7e38578807189e134dda9ccb649
safi100 <safinafi12@gmail.com>
2024-06-10 22:28:10 +0300

    update admin model

--- backend/models/admin.js
+++ backend/models/admin.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const AdminSchema = new Schema({
-    username: {
+    email: {
         type: String,
         required: true,
         unique: true
