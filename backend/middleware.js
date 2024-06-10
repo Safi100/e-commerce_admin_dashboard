dd9fdedb2ee51f545b54f896ced60e0ad14dd3dd
safi100 <safinafi12@gmail.com>
2024-06-10 22:47:24 +0300

    fix cookie name

--- backend/middleware.js
+++ backend/middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 module.exports.authenticateJWT = (req, res, next) => {
     const secret_key = process.env.SECRET_KEY
-    const token = req.headers.authorization || req.cookies.access_token;
+    const token = req.headers.authorization || req.cookies.accessToken;
     if (!token) return res.status(401).send('You must log in to access this');
     jwt.verify(token, secret_key, (err, user) => {
         if (err) return res.status(403).json({ error: 'Token is not valid!' });
