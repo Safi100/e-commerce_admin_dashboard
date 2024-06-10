9b2dfff792d99a5e356ea49b3d376f996e2b61b5
safi100 <safinafi12@gmail.com>
2024-06-11 00:33:16 +0300

    add logout route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { login, fetchCurrentUser } = require('../controllers/auth')
+const { login, fetchCurrentUser, logout } = require('../controllers/auth')
 const router = express.Router({mergeParams: true})
 const { authenticateJWT } = require('../middleware')
 
 router.post('/login', login)
 
 router.get('/currentUser', authenticateJWT, fetchCurrentUser)
+
+router.post('/logout', logout)
 
 module.exports = router
