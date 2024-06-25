2cad57035f4981f4902a0f4a4ba27acd38831b19
safi100 <safinafi12@gmail.com>
2024-06-25 12:38:38 +0300

    remove log

--- backend/controllers/advertisement.js
+++ backend/controllers/advertisement.js
@@ -21,7 +21,6 @@ module.exports.deleteAdvertisements =  async (req, res) => {
     try{
         const DeletedAdvertisement = await Advertisement.find({ _id: { $in: req.body } })
         DeletedAdvertisement.forEach(async Advertisement => {
-            console.log(Advertisement.image.filename);
             await cloudinary.uploader.destroy(Advertisement.image.filename)
         });
         await Advertisement.deleteMany({_id: {$in: req.body} })
